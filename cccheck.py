09619d94cb47f990291c3dc117464f9957b33ac6
minesatour <161233665+minesatour@users.noreply.github.com>
2024-05-01 23:56:32 +0100

    Update cccheck.py

--- cccheck.py
+++ cccheck.py
@@ -8,14 +8,13 @@ CARDS_FILE = "cards.txt"
 BIN_FILE = "BIN.txt"
 VALID_CARDS_FILE = "valid.txt"
 
-# Regular expression pattern for validating URLs
 URL_PATTERN = re.compile(
-    r'^(?:http|ftp)s?://'  # http:// or https://
-    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
-    r'localhost|'  # localhost...
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
-    r'(?::\d+)?'  # optional port
-    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    r'^(?:http|ftp)s?://'  # http:// or https://
+    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+    r'localhost|'  # localhost...
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+    r'(?::\d+)?'  # optional port
+    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
 # Load data from files
 def load_file(filename):
